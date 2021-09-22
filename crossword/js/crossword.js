2617b5d979f6dfcae5572adbdbe3b753c154b93d
jose <jose.p.restrepo@gmail.com>
2021-09-21 22:15:04 -0500

    issue fixed

--- crossword/js/crossword.js
+++ crossword/js/crossword.js
@@ -687,13 +687,13 @@ function resize() {
   }
 
   // gridContainer.removeAllChildren();
-  for (var x = 0; x < cellMap.length; x++) {
-    for (var y = 0; y < cellMap[x].length; y++) {
-      if (cellMap[x][y]) {
-        cellMap[x][y] = null;
-      }
-    }
-  }
+  // for (var x = 0; x < cellMap.length; x++) {
+  //   for (var y = 0; y < cellMap[x].length; y++) {
+  //     if (cellMap[x][y]) {
+  //       cellMap[x][y] = null;
+  //     }
+  //   }
+  // }
 
   setCanvasDimensions();
   createCrosswordGrid();
